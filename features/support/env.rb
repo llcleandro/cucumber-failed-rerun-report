ff7df9a66f93e44351b3fba4e7eac994a89286bd
Leandro Coelho <leandro.coelho@walmart.com>
2016-09-08 14:42:26 -0300

    added more options of browsers

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,15 +5,34 @@ require "rspec"
 require 'capybara/poltergeist'
 require 'capybara/rspec'
 require 'httparty'
-#require 'pry'
+# require 'pry'
 # require 'capybara/accessible'
 
-Capybara.default_driver = :chrome
+## Loading browser defined (BROWSER=pg / BROWSER=f / without parameter chrome)
 
-
-Capybara.register_driver :chrome do |app|
-	Capybara::Selenium::Driver.new(app, :browser => :chrome)
+case ENV['BROWSER']
+	when 'pg'
+		# using poltergeist
+		DRIVER = :poltergeist
+		Capybara.register_driver :poltergeist_debug do |app|
+		  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--ignore-ssl-errors=yes'])
+		end
+	when 'ff'
+		# using firefox
+		DRIVER = :firefox
+		Capybara.register_driver :firefox do |app|
+			Capybara::Selenium::Driver.new(app, :browser => :firefox)
+		end		
+	else
+		# using chrome
+		DRIVER = :chrome
+		Capybara.register_driver :chrome do |app|
+			Capybara::Selenium::Driver.new(app, :browser => :chrome)
+		end
 end
+
+Capybara.default_driver = DRIVER
+Capybara.default_wait_time = 20
 
 After do |scenario|
   if(scenario.failed?)
